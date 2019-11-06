80be19c30e9635005ca8774eee980476c9d3e954
Karl Sander <kall@kall.ws>
2019-11-06 16:24:40 +0100

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { jsx as emotion } from '@emotion/core';
-import css from '@styled-system/css';
+import css, {
+  ResponsiveStyleValue,
+  SystemStyleObject,
+} from '@styled-system/css';
 
 function makePragma<T>(theme: T) {
   return (type: any, props: any, ...children: any[]) =>
@@ -11,3 +14,43 @@ function makePragma<T>(theme: T) {
 }
 
 export { makePragma };
+
+/**
+ * The `SxStyleProp` extension `SystemStyleObject` and `Emotion` [style props](https://emotion.sh/docs/object-styles)
+ * such that properties that are part of the `Theme` will be transformed to
+ * their corresponding values. Other valid CSS properties are also allowed.
+ */
+type SxStyleProp = SystemStyleObject &
+  Record<
+    string,
+    | SystemStyleObject
+    | ResponsiveStyleValue<number | string>
+    | Record<string, SystemStyleObject | ResponsiveStyleValue<number | string>>
+  >;
+
+interface SxProps {
+  /**
+   * The sx prop lets you style elements inline, using values from your
+   * theme. To use the sx prop, add the custom pragma as a comment to the
+   * top of your module and import the jsx function.
+   *
+   * ```ts
+   * // @jsx jsx
+   *
+   * import { jsx } from 'theme-ui'
+   * ```
+   */
+  sx?: SxStyleProp;
+}
+
+declare module 'react' {
+  // tslint:disable-next-line: no-empty-interface
+  interface DOMAttributes<T> extends SxProps {}
+}
+
+declare global {
+  namespace JSX {
+    // tslint:disable-next-line: no-empty-interface
+    interface IntrinsicAttributes extends SxProps {}
+  }
+}
